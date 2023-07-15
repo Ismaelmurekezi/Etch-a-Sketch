2ba683f15fab824ed06aca5110d29113c91c95cb
MUREKEZI Ismael <ismaelmurekezi1@gmail.com>
2023-07-15 19:44:51 +0200

    Adding rainbow button,its style and it functionality

--- index.js
+++ index.js
@@ -1,102 +1,13 @@
-
-
-// const container = document.querySelector(".container");
-// const gridSizeButton = document.querySelector(".grid-size-button");
-// const clearButton = document.querySelector(".clear-button");
-// const resetButton = document.querySelector(".reset-button");
-// const colorButtons = document.querySelectorAll(".color-button");
-
-// let gridSize = 16;
-// let currentColor = "blue";
-
-// // Function to create the initial grid
-// function createGrid(size) {
-//   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-//   container.innerHTML = "";
-
-//   for (let i = 0; i < size * size; i++) {
-//     const gridCell = document.createElement("div");
-//     gridCell.classList.add("grid-cell");
-//     container.appendChild(gridCell);
-//   }
-//   // Event listener for the grid size button
-// gridSizeButton.addEventListener("click", function changeGridSize() {
-//   let newGridSize = prompt("Enter grid size (between 2 and 100):");
-//   newGridSize = parseInt(newGridSize);
-
-//   if (newGridSize >= 2 && newGridSize <= 100 && !isNaN(newGridSize)) {
-//     gridSize = newGridSize;
-//     createGrid(gridSize);
-//   } else {
-//     alert("Invalid grid size! Please enter a number between 2 and 100.");
-//   }
-// });
-
-//   // Add event listeners to the grid cells
-//   const gridCells = document.querySelectorAll(".grid-cell");
-//   gridCells.forEach((cell) => {
-//     cell.addEventListener("mouseenter", applyColor);
-//   });
-// }
-
-// // Function to apply the selected color or erase the grid cell
-// function applyColor(e) {
-//   const cell = e.target;
-//   if (clearButton.classList.contains("active")) {
-//     cell.style.backgroundColor = "";
-//   } else {
-//     cell.style.backgroundColor = currentColor;
-//   }
-// }
-
-
-
-
-// // Function to clear the grid cells
-// function clearGrid() {
-//   const gridCells = document.querySelectorAll(".grid-cell");
-//   gridCells.forEach((cell) => {
-//     cell.style.backgroundColor = "";
-//   });
-// }
-
-
-
-
-
-// // Event listeners for the color buttons
-// colorButtons.forEach((button) => {
-//   button.addEventListener("click", (e) => {
-//     currentColor = e.target.dataset.color;
-//     clearButton.classList.remove("active");
-//   });
-// });
-
-// // Event listener for the clear button
-// clearButton.addEventListener("click", () => {
-//   clearButton.classList.toggle("active");
-// });
-
-// // Event listener for the reset button
-// resetButton.addEventListener("click",function resetGrid() {
-//   gridSize = 16;
-//   createGrid(gridSize);
-//   clearGrid();
-// }
-//  );
-
-// // Create the initial grid
-// createGrid(gridSize);
-
-
 const container = document.querySelector(".container");
 const gridSizeButton = document.querySelector(".grid-size-button");
 const clearButton = document.querySelector(".clear-button");
 const resetButton = document.querySelector(".reset-button");
+const rainbowButton = document.querySelector(".rainbow-button");
 const colorButtons = document.querySelectorAll(".color-button");
 
 let gridSize = 16;
 let currentColor = "blue";
+let isRainbowEnabled = false;
 
 // Function to create the initial grid
 function createGrid(size) {
@@ -116,26 +27,22 @@ function createGrid(size) {
   });
 }
 
-// Function to apply the selected color or erase the grid cell
+// Function to apply the selected color to a grid cell
 function applyColor(e) {
   const cell = e.target;
-  if (clearButton.classList.contains("active")) {
+  if (isRainbowEnabled) {
+    const randomHue = Math.floor(Math.random() * 360);
+    const randomColor = `hsl(${randomHue}, 100%, 50%)`;
+    cell.style.backgroundColor = randomColor;
+  } else if (clearButton.classList.contains("active")) {
     cell.style.backgroundColor = "";
   } else {
     cell.style.backgroundColor = currentColor;
   }
 }
 
-// Function to clear the grid cells
-function clearGrid() {
-  const gridCells = document.querySelectorAll(".grid-cell");
-  gridCells.forEach((cell) => {
-    cell.style.backgroundColor = "";
-  });
-}
-
-// Event listener for the grid size button
-gridSizeButton.addEventListener("click", () => {
+// Function to change the grid size
+function changeGridSize() {
   let newGridSize = prompt("Enter grid size (between 2 and 100):");
   newGridSize = parseInt(newGridSize);
 
@@ -145,15 +52,40 @@ gridSizeButton.addEventListener("click", () => {
   } else {
     alert("Invalid grid size! Please enter a number between 2 and 100.");
   }
-});
+}
+
+
+
+// Function to clear the grid cells
+function clearGrid() {
+  const gridCells = document.querySelectorAll(".grid-cell");
+  gridCells.forEach((cell) => {
+    cell.style.backgroundColor = "";
+  });
+}
+
+// Function to reset the grid size and clear the grid cells
+function resetGrid() {
+  gridSize = 16;
+  createGrid(gridSize);
+  clearGrid();
+}
+
+// Event listener for the grid size button
+gridSizeButton.addEventListener("click", changeGridSize);
 
 // Event listeners for the color buttons
 colorButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
+    if (isRainbowEnabled) {
+      isRainbowEnabled = false;
+      rainbowButton.classList.remove("active");
+    }
     currentColor = e.target.dataset.color;
     clearButton.classList.remove("active");
   });
 });
+
 
 // Event listener for the clear button
 clearButton.addEventListener("click", () => {
@@ -161,12 +93,13 @@ clearButton.addEventListener("click", () => {
 });
 
 // Event listener for the reset button
-resetButton.addEventListener("click", () => {
-  gridSize = 16;
-  createGrid(gridSize);
-  clearGrid();
+resetButton.addEventListener("click", resetGrid);
+
+// Event listener for the rainbow button
+rainbowButton.addEventListener("click", () => {
+  isRainbowEnabled = !isRainbowEnabled;
+  rainbowButton.classList.toggle("active");
 });
 
 // Create the initial grid
 createGrid(gridSize);
-
